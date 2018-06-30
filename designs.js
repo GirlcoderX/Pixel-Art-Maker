c1a13c4a2b9bd5a118e7954856a80a20ab425a15
girlcoderx <nwosumg@gmail.com>
2018-06-30 17:33:10 +0100

    feat: create grid
    using the inputHeight and inputWeight supplied, create a grid of MxN number of cells.

--- designs.js
+++ designs.js
@@ -5,6 +5,24 @@
 
 function makeGrid() {
 
-// Your code goes here!
+    // Your code goes here!
+    let height, weight;
 
+    height = $("#inputHeight").val();
+    weight = $("#inputWeight").val();
+
+    let grid = "";
+    // Create a row by looping through the value of #inputHeight
+    for (let i = 1; i <= height; i++) {
+        grid += "<tr>"; // Open table row
+        for (let j = 1; j <= weight; j++) {
+            grid += "<td></td>" // Append cell to row
+        }
+        grid += "</tr>"; // Close table row
+    };
+
+    //Empties the old grid then replaces it with the new grid on submit
+    $("#pixelCanvas").empty().append(grid);
 }
+
+$("#sizePicker").submit(makeGrid);
