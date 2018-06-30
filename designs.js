22e4a5dab54c821bbb40da127508d7b156aa4fd4
girlcoderx <nwosumg@gmail.com>
2018-06-30 17:48:27 +0100

    feat: toggle cell color
    Add ability to be toggle cell background color.

--- designs.js
+++ designs.js
@@ -24,6 +24,22 @@ function makeGrid() {
     //Empties the old grid then replaces it with the new grid on submit
     $("#pixelCanvas").empty().append(grid);
 
+    //Assign a default color of the grid white
+    const defaultColor = "rgb(255, 255, 255)";
+    $("td").css("background-color", defaultColor);
+
+    // When clicked toggle between default color and selected color
+    $("td").click(function () {
+        let color = $("#colorPicker").val(); // Create variable color to hold the current value of the color picker
+
+        // Check if the color of the clicked cell is the default color and assign
+        if ($(this).css("background-color") !== defaultColor) {
+            $(this).css("background-color", defaultColor);
+        } else {
+            $(this).css('background-color', color)
+        };
+    });
+
     //Return false to prevent default action taken when the form is submitted
     return false;
 }
